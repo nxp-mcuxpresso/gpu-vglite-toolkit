b785a6bd36a750c5eefa06928cf64a991be721d2
Nilam Gaikwad <nilam.gaikwad@nxp.com>
2024-10-09 18:13:45 +0530

    MGG-1425 [#ccc] script: Add gpu-vglite-toolkit.sh
    Add a generic script to transform SVG file to header.
    
    Add internal and external svgpathtools references
      ssh://git@bitbucket.sw.nxp.com/gtec/svgpathtools.git
         This is NXP's improvement in svgpathtools
    
      https://github.com/mathandy/svgpathtools.git
         This is original version of svgpathtools
         At somepoint in future, NXP can share patches
         from bitbucket branch(vglite-tools-next)
         to this repository.
    
    Signed-off-by: Nilam Gaikwad <nilam.gaikwad@nxp.com>

---
+++ gpu-vglite-toolkit.sh
@@ -0,0 +1,20 @@
+INPUT_FILE=$1
+
+if [ ! -x $PWD/svgpathtools ]; then
+    # External public MIT licensed repository
+    #git clone https://github.com/mathandy/svgpathtools.git
+
+    # Internal NXP development repository
+    git clone -b vglite-tools-next ssh://git@bitbucket.sw.nxp.com/gtec/svgpathtools.git
+fi
+
+if [ ! -z "$2" ]; then
+        OUTPUT_FILE=$2
+else
+        OUTPUT_FILE="svgt12_test.h"
+fi
+
+sed 's/xml:id/id/g' $INPUT_FILE > svgt12_test.svg
+
+export PYTHONPATH=$PYTHONPATH:$PWD/svgpathtools
+python3 svg2cKPI.py svgt12_test.svg > "../$OUTPUT_FILE"
