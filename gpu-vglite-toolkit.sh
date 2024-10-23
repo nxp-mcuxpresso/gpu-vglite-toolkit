ee6d753686b35139e7e61c399785bb820d60acee
Nilam Gaikwad <nilam.gaikwad@nxp.com>
2024-10-23 13:55:10 +0530

    MGG-1425 [#ccc] gpu-vglite-toolkit.sh: Added usage documentation
    Signed-off-by: Nilam Gaikwad <nilam.gaikwad@nxp.com>

--- gpu-vglite-toolkit.sh
+++ gpu-vglite-toolkit.sh
@@ -1,5 +1,27 @@
+#!/bin/bash
 
-INPUT_FILE=/opt/BUILD/GTEC/VGLITE/SVGT12/$1
+#
+# This is top-level script to use gpu-vglite-toolkit applicaiton in Linux.
+# gpu-vglite-toolkit application uses svgpathtools as third-party component
+# To parse SVG files. NXP has done some improvements in this open-source library
+# and they need to be at specific path for gpu-vglite-toolkit to operate.
+# As a result on 1st execution of this script, it setup's necessary environment.
+#
+# Usage:
+#  gpu-vglite-tests.sh paint-color-01-t.svg
+#     Without output filename it generartes svgt12_test.h
+#     e.g. 
+#        svgt12_test.h as output heaader with graphics artifacts
+#        paint-color-01-t.err for path summary, and any error during SVG to header conversion
+#
+#  gpu-vglite-tests.sh paint-color-01-t.svg paint-color-01-t.h
+#     With output file name it generated user-specified output file.
+#     e.g. 
+#        paint-color-01-t.h as output heaader with graphics artifacts
+#        paint-color-01-t.err for path summary, and any error during SVG to header conversion
+#
+
+INPUT_FILE=$1
 
 if [ ! -x $PWD/svgpathtools ]; then
     # External public MIT licensed repository
@@ -15,14 +37,14 @@ else
         OUTPUT_FILE="svgt12_test.h"
 fi
 
-sed 's/xml:id/id/g' $INPUT_FILE > svgt12_test.svg
-
-echo ${INPUT_FILE}
-OUT_NAME=$(echo $(basename ${INPUT_FILE}) | sed 's/\.svg/\.h/')
-
+# Name of output file as header
+#OUT_NAME=$(echo $(basename ${INPUT_FILE}) | sed 's/\.svg/\.h/')
+# File to capture errors from 
 OUT_ERR=$(echo $(basename ${INPUT_FILE}) | sed 's/\.svg/\.err/')
+
+# Property setup svgpathtools module path
 export PYTHONPATH=$PYTHONPATH:$PWD/svgpathtools
-python3 svg2cKPI.py svgt12_test.svg > "$OUTPUT_FILE" 2>${OUT_ERR}
-cp -v ${OUTPUT_FILE} /opt/BUILD/GTEC/VGLITE_FINAL/MCUSDK/source/${OUTPTU_FILE}
-mv -fv ${OUTPUT_FILE} R5_header_file/${OUT_NAME}
-mv -fv ${OUT_ERR} R5_header_file/${OUT_ERR}
+
+# Actual SVG -> header Conversion
+python3 svg2cKPI.py ${INPUT_FILE} 1>"${OUTPUT_FILE}" 2>"${OUT_ERR}"
+
